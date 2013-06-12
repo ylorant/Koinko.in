b2fb761fa1e7194abac50484f23acc452706535e
Yohann Lorant <yohann.lorant@gmail.com>
2013-06-13 00:24:04 +0200

    Added link to source code Github + htaccess to repo.

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,7 +7,7 @@
 		<a onclick="koinkoin.showMyLinks();">My links</a>
 	</div>
 	<div class="bottom-links right">
-		&copy; 2012 Koinko.in
+		&copy; 2012 Koinko.in - <a href="http://github.com/ylorant/Koinko.in">Source code</a>
 		<?php if($config['debug']): ?>
 			- <a class="debug-window-toggle" onclick="$('.debug-window').toggle();">Toggle debug window</a>
 		<?php endif; ?>
